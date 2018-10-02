88212baaef6af85af9dc7efca93f8beaf06433a5
Cody A <codyanagnost@hotmail.com>
2018-10-02 11:19:36 -0400

    Finished "Favorite" working on "Password"

--- Favorite/Program.cs
+++ Favorite/Program.cs
@@ -6,7 +6,13 @@ namespace Favorite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your favorite food? ");
+            String food = Console.ReadLine();
+
+            if (food != "pizza")
+                Console.WriteLine("Ew discusting thats gross i cant believe youd even say that like ew thats so gross ima gonna gag. Youre fav food should be pizza!! ");
+            else
+                Console.WriteLine("Very good choice boi");
         }
     }
 }
---
+++ Password/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int att = 0;
+
+            Console.WriteLine("Input a username loser ");
+            String username = Console.ReadLine();
+            if(username != "root")
+                Console.WriteLine("well thats not right");
+            att++;
+
+
+            Console.WriteLine("Now gimmy ya shword ");
+            String shword = Console.ReadLine();
+            if (shword != "letmein")
+                Console.WriteLine("well thats still not right");
+            att++;
+
+            if (att == 3)
+                
+              Console.WriteLine("ur locked out fool");
+           
+        }
+    }
+}
