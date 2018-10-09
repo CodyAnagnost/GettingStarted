889595ba24a56bca7588b3930281b696f18a81d7
Cody A <codyanagnost@hotmail.com>
2018-10-09 11:25:27 -0400

    mhm

--- Password/Program.cs
+++ Password/Program.cs
@@ -8,24 +8,41 @@ namespace Password
         {
 
             int att = 0;
+            while (att < 3) 
+            {
+                Console.WriteLine("Input a username loser ");
+                String username = Console.ReadLine();
+                if (username != "root")
+                {
+                    Console.WriteLine("well thats not right");
+                    att++;
+                }
 
-            Console.WriteLine("Input a username loser ");
-            String username = Console.ReadLine();
-            if(username != "root")
-                Console.WriteLine("well thats not right");
-            att++;
+                else
+                {
+                    Console.WriteLine("Correct");
+                }
 
 
-            Console.WriteLine("Now gimmy ya shword ");
-            String shword = Console.ReadLine();
-            if (shword != "letmein")
-                Console.WriteLine("well thats still not right");
-            att++;
+                Console.WriteLine("Now gimmy ya shword ");
+                String shword = Console.ReadLine();
+                if (shword != "letmein")
+                {
+                    Console.WriteLine("well thats still not right");
+                    att++;
+                }
+                else
+                {
+                    Console.WriteLine("uR in Kid");
+                    break;
+                }
+            }
 
             if (att == 3)
-                
-              Console.WriteLine("ur locked out fool");
-           
+
+            {
+                Console.WriteLine("ur locked out fool");
+            }
         }
     }
 }
